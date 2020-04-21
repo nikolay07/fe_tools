c56b49dcd0fb9d1895c322528ad9985360cf492e
nikolay07 <nikolay0701@ukr.net>
2020-04-21 18:06:46 +0300

    1

---
+++ lesson1/task1/dist/scripts/calculator/__tests__/calculator.spec.js
@@ -0,0 +1,13 @@
+import { sum, mult } from '../calculator';
+
+it('Should return 8', () => {
+    const result = sum(3, 5);
+
+    expect(result).toEqual(8);
+});
+
+it('Should return 15', () => {
+    const result = mult(3, 5);
+
+    expect(result).toEqual(15);
+});
