08d1ba103a5c1fb3276aacb9024e76e3ee22a73d
nikolay07 <nikolay0701@ukr.net>
2020-04-24 14:48:10 +0300

    1

--- lesson7/hw1/webpack.config.js
+++ lesson7/hw1/webpack.config.js
@@ -2,38 +2,38 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack');
-
 module.exports = {
-    entry: './src/index.js',
+    entry: "./scripts/index.js",
     output: {
-        filename: 'bundle.js'
+        filename: "bundle.js"
     },
     module: {
         rules: [{
                 test: /.js$/,
                 use: ['babel-loader']
-            }, {
+            },
+            {
                 test: /.s?css$/,
                 use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
             },
             {
-                test: /.jpg|png$/,
+                test: /\.(png|jpg|gif)$/,
                 use: [{
                     loader: 'url-loader',
                     options: {
-                        limit: 8,
+                        limit: 8192,
                         name: '[name].[ext]',
                         outputPath: 'images',
                     },
                 }, ],
             }
-        ]
+        ],
     },
     plugins: [
         new webpack.ProgressPlugin(),
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            template: './src/index.html'
+            template: './index.html'
         }),
         new MiniCssExtractPlugin({
             filename: '[name].css',
