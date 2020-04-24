96c72120a538b02e01ccee210f4c3438bea528a5
nikolay07 <nikolay0701@ukr.net>
2020-04-24 15:08:52 +0300

    1

---
+++ lesson8/task1/webpack.config.js
@@ -0,0 +1,53 @@
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === 'production';
+
+    const config = {
+        entry: "./src/index.js",
+        output: {
+            filename: "bundle.js"
+        },
+        module: {
+            rules: [{
+                    test: /.s?css$/,
+                    use: [
+                        isProduction ?
+                        MiniCssExtractPlugin.loader :
+                        "style-loader",
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                },
+                {
+                    test: /\.(png|jpg|gif)$/,
+                    use: [{
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                            name: '[name].[ext]',
+                            outputPath: 'images',
+                        },
+                    }, ],
+                }
+            ],
+        },
+        plugins: [
+            new htmlWebpackPlugin({
+                template: './src/index.html'
+            })
+        ],
+        devServer: {
+            port: 9000,
+            hot: true,
+        }
+    };
+    if (isProduction) {
+        config.plugins.push(new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }));
+    }
+
+    return config;
+}
