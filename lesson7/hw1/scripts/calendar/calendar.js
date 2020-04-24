d942d1d9f8b151f63dc02741def85af9c6e685ee
nikolay07 <nikolay0701@ukr.net>
2020-04-24 12:40:26 +0300

    1

---
+++ lesson7/hw1/scripts/calendar/calendar.js
@@ -0,0 +1,27 @@
+import { getItem } from '../common/storage.js';
+import { generateWeekRange, createNumbersArray } from '../common/time.utils.js';
+import { renderEvents } from '../events/events.js';
+
+const week = document.querySelector('.calendar__week');
+const generateDay = () => {
+    // функция должна сгенерировать и вернуть разметку недели в виде строки
+    // разметка состоит из 24 часовых временных слотов (.calendar__time-slot)
+    const time = createNumbersArray(0, 23)
+        .map(elem => `<div class="calendar__time-slot" data-time="${elem}"></div>`)
+        .join('');
+    return time;
+};
+
+export const renderWeek = () => {
+    // функция должна сгенерировать разметку недели в виде строки и вставить ее на страницу (в .calendar__week)
+    // разметка недели состоит из 7 дней (.calendar__day) отображаемой недели
+    // каждый день должен содержать в дата атрибуте порядковый номер для в месяце
+    // какую неделю отображать - берем из storage
+    // после того, как отрисовали всю сетку для отображаемой недели - добавляем события методом renderEvents
+    const days = createNumbersArray(1, 7)
+        .map(elem => `<div class="calendar__day" data-day="${elem}">
+                            ${generateDay()}
+                    </div>`)
+        .join('');
+    week.innerHTML = days;
+};
---
+++ lesson7/hw1/scripts/events/createEvent.js
@@ -0,0 +1,28 @@
+import { getItem, setItem } from '../common/storage.js';
+import { renderEvents } from './events.js';
+import { getDateTime } from '../common/time.utils.js';
+//import { closeModal } from '../common/modal.js';
+
+const eventFormElem = document.querySelector('.event-form');
+const closeEventFormBtn = document.querySelector('.create-event__close-btn');
+
+function clearEventForm() {
+    // ф-ция должна очистить поля формы от значений
+}
+
+function onCloseEventForm() {
+    // при закрытии модального окна нужно очистить форму
+}
+
+function onCreateEvent(event) {
+    // при подтверждении формы нужно считать данные с формы
+    // на основе полей date, startTime, endTime нужно посчитать дату начала и окончания события
+    // поможет ф-ция getDateTime из утилит
+    // полученное событие добавляем в массив событий, что хранится в storage
+    // и запускаем перерисовку событий
+}
+
+
+export function initEventForm() {
+    // подпишитесь на сабмит формы и на закрытие формы
+}
