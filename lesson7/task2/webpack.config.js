d2f94394ed1cbf9931bdb6f0ca87e08e4c8ef73a
nikolay07 <nikolay0701@ukr.net>
2020-04-24 13:54:42 +0300

    1

--- lesson7/task2/webpack.config.js
+++ lesson7/task2/webpack.config.js
@@ -1,3 +1,8 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
 module.exports = {
     entry: "./src/scripts/index.js",
     output: {
@@ -24,5 +29,15 @@ module.exports = {
                 }, ],
             }
         ],
-    }
+    },
+    plugins: [
+        new webpack.ProgressPlugin(),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
+    ]
 }
